6182d8b701be7aa9e5a3681ab18883b0c4d5dc74
Jon Sadka <jonsadka@gmail.com>
2014-09-03 09:09:49 -0700

    updated chck interval to 10 minutes and removed airport data

--- app.js
+++ app.js
@@ -15,24 +15,15 @@ db.points = new Datastore({ filename: 'db/points', autoload: true });
 db.routes = new Datastore({ filename: 'db/routes', autoload: true });
 db.prices = new Datastore({ filename: 'db/prices', autoload: true });
 
-var routes =[{start:"lax", end:"dtla"},{start:"dtla", end:"lax"},
- {start:"lax", end:"sm"},{start:"sm", end:"lax"},
- {start:"lax", end:"hwd"},{start:"hwd", end:"lax"},
- {start:"dtla", end:"sm"},{start:"sm", end:"dtla"},
- {start:"dtla", end:"hwd"},{start:"hwd", end:"dtla"},
- {start:"sm", end:"hwd"},{start:"sm", end:"lax"},
- {start:"jfk", end:"gct"},{start:"gct", end:"jfk"},
- {start:"jfk", end:"aaal"},{start:"aaal", end:"jfk"},
- {start:"jfk", end:"brky"},{start:"brky", end:"jfk"},
- {start:"gct", end:"aaal"},{start:"aaal", end:"gct"},
- {start:"gct", end:"brky"},{start:"brky", end:"gct"},
- {start:"aaal", end:"brky"},{start:"aaal", end:"jfk"},
- {start:"sfo", end:"acs"},{start:"acs", end:"sfo"},
- {start:"sfo", end:"pwll"},{start:"pwll", end:"sfo"},
- {start:"sfo", end:"warf"},{start:"warf", end:"sfo"},
- {start:"acs", end:"pwll"},{start:"pwll", end:"acs"},
- {start:"acs", end:"warf"},{start:"warf", end:"acs"},
- {start:"pwll", end:"warf"},{start:"pwll", end:"sfo"}]
+var routes =[{start:"dtla", end:"sm"},{start:"sm", end:"dtla"},
+						 {start:"dtla", end:"hwd"},{start:"hwd", end:"dtla"},
+						 {start:"sm", end:"hwd"},
+						 {start:"gct", end:"aaal"},{start:"aaal", end:"gct"},
+						 {start:"gct", end:"brky"},{start:"brky", end:"gct"},
+						 {start:"aaal", end:"brky"},
+						 {start:"acs", end:"pwll"},{start:"pwll", end:"acs"},
+						 {start:"acs", end:"warf"},{start:"warf", end:"acs"},
+						 {start:"pwll", end:"warf"}];
 
 app.get('/',function(req, res){
   res.render('index');  
@@ -40,29 +31,23 @@ app.get('/',function(req, res){
 
 //Seed database
 app.get('/seed',function(req,res){
-	// db.points.insert({name:"San Francisco Downtown",slug:"sf_downtown",lat:"37.7833", lon:"-122.4167"});
-	// db.points.insert({name:"San Francisco Airport",slug:"sf_airport",lat:"37.7833", lon:"-122.4167"});
-	db.points.insert({name:"LAX (Los Angeles International Airport)",slug:"lax",lat:"33.945452",lon:"-118.399974"})
-db.points.insert({name:"Downtown LA (Walt Disney Concert Hall)",slug:"dtla",lat:"34.055515",lon:"-118.250039"})
-db.points.insert({name:"Santa Monica (Third Street Promenade)",slug:"sm",lat:"34.016243",lon:"-118.496159"})
-db.points.insert({name:"Hollywood (Mann Theatres)",slug:"hwd",	lat:"34.102298",lon:"-118.340992"})
-			
-db.points.insert({name:"JFK (John F. Kennedy International Airport)",slug:"jfk",lat:"40.655839",lon:"-73.807594"})
-db.points.insert({name:"Manhattan (Grand Central Terminal)",slug:"gct",lat:"40.752466",lon:"-73.976886"})
-db.points.insert({name:"Upper Manhattan (American Academy of Arts and Letters)",slug:"aaal",lat:"40.833721",lon:"-73.947461"})
-db.points.insert({name:"Brooklyn (Barclays Center)",slug:"brky",lat:"40.682907",lon:"-73.975255"})
-			
-db.points.insert({name:"SFO (San Francisco International Airport)",slug:"sfo",lat:"37.625732",lon:"-122.377807"})
-db.points.insert({name:"Golden Gate Park (California Academy of Sciences)",slug:"acs",lat:"37.770094",lon:"-122.466031"})
-db.points.insert({name:"Downtown SF (Powell & Market)",slug:"pwll",lat:"37.785114",lon:"-122.406677"})
-db.points.insert({name:"Fishermans Warf (Pier 39)",slug:"warf",lat:"37.808119",lon:"-122.40911"})
-
+	db.points.insert({name:"Downtown LA (Walt Disney Concert Hall)",slug:"dtla",lat:"34.055515",lon:"-118.250039"})
+	db.points.insert({name:"Santa Monica (Third Street Promenade)",slug:"sm",lat:"34.016243",lon:"-118.496159"})
+	db.points.insert({name:"Hollywood (Mann Theatres)",slug:"hwd",	lat:"34.102298",lon:"-118.340992"})
+				
+	db.points.insert({name:"Manhattan (Grand Central Terminal)",slug:"gct",lat:"40.752466",lon:"-73.976886"})
+	db.points.insert({name:"Upper Manhattan (American Academy of Arts and Letters)",slug:"aaal",lat:"40.833721",lon:"-73.947461"})
+	db.points.insert({name:"Brooklyn (Barclays Center)",slug:"brky",lat:"40.682907",lon:"-73.975255"})
+				
+	db.points.insert({name:"Golden Gate Park (California Academy of Sciences)",slug:"acs",lat:"37.770094",lon:"-122.466031"})
+	db.points.insert({name:"Downtown SF (Powell & Market)",slug:"pwll",lat:"37.785114",lon:"-122.406677"})
+	db.points.insert({name:"Fishermans Warf (Pier 39)",slug:"warf",lat:"37.808119",lon:"-122.40911"})
 });
 
 //Launch crawler
 app.get('/launch',function(req,res){
 	var rule = new schedule.RecurrenceRule();
-	rule.minute = new schedule.Range(0, 60, 15);
+	rule.minute = new schedule.Range(0, 60, 10);
 
 	var j = schedule.scheduleJob(rule, function () {
 	  // Do something
