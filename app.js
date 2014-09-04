f2559edbce3b3fa608d8778551aaa6db9396cda7
Nicolas Grenié <nicolas.grenie@gmail.com>
2014-09-04 15:42:53 -0700

    polishing code

--- app.js
+++ app.js
@@ -42,27 +42,13 @@ app.get('/',function(req, res){
 app.get('/seed',function(req,res){
 	// db.points.insert({name:"San Francisco Downtown",slug:"sf_downtown",lat:"37.7833", lon:"-122.4167"});
 	// db.points.insert({name:"San Francisco Airport",slug:"sf_airport",lat:"37.7833", lon:"-122.4167"});
-	db.points.insert({name:"LAX (Los Angeles International Airport)",slug:"lax",lat:"33.945452",lon:"-118.399974"})
-db.points.insert({name:"Downtown LA (Walt Disney Concert Hall)",slug:"dtla",lat:"34.055515",lon:"-118.250039"})
-db.points.insert({name:"Santa Monica (Third Street Promenade)",slug:"sm",lat:"34.016243",lon:"-118.496159"})
-db.points.insert({name:"Hollywood (Mann Theatres)",slug:"hwd",	lat:"34.102298",lon:"-118.340992"})
-			
-db.points.insert({name:"JFK (John F. Kennedy International Airport)",slug:"jfk",lat:"40.655839",lon:"-73.807594"})
-db.points.insert({name:"Manhattan (Grand Central Terminal)",slug:"gct",lat:"40.752466",lon:"-73.976886"})
-db.points.insert({name:"Upper Manhattan (American Academy of Arts and Letters)",slug:"aaal",lat:"40.833721",lon:"-73.947461"})
-db.points.insert({name:"Brooklyn (Barclays Center)",slug:"brky",lat:"40.682907",lon:"-73.975255"})
-			
-db.points.insert({name:"SFO (San Francisco International Airport)",slug:"sfo",lat:"37.625732",lon:"-122.377807"})
-db.points.insert({name:"Golden Gate Park (California Academy of Sciences)",slug:"acs",lat:"37.770094",lon:"-122.466031"})
-db.points.insert({name:"Downtown SF (Powell & Market)",slug:"pwll",lat:"37.785114",lon:"-122.406677"})
-db.points.insert({name:"Fishermans Warf (Pier 39)",slug:"warf",lat:"37.808119",lon:"-122.40911"})
-
+	seedDB();
 });
 
 //Launch crawler
 app.get('/launch',function(req,res){
 	var rule = new schedule.RecurrenceRule();
-	rule.minute = new schedule.Range(0, 60, 15);
+	rule.minute = new schedule.Range(0, 60, 10);
 
 	var j = schedule.scheduleJob(rule, function () {
 	  // Do something
@@ -91,7 +77,6 @@ app.get('/prices/:start/:end',function(req,res){
 
 function getDataFromUber(){
 	console.log('called',Date.now());
-	
 
 	async.each(routes,function(item){
 		console.log(item.start, item.end);
@@ -121,6 +106,23 @@ function getDataFromUber(){
 		// request.get('')
 	})
 	
+}
+
+function seedDB(){
+	db.points.insert({name:"LAX (Los Angeles International Airport)",slug:"lax",lat:"33.945452",lon:"-118.399974"})
+	db.points.insert({name:"Downtown LA (Walt Disney Concert Hall)",slug:"dtla",lat:"34.055515",lon:"-118.250039"})
+	db.points.insert({name:"Santa Monica (Third Street Promenade)",slug:"sm",lat:"34.016243",lon:"-118.496159"})
+	db.points.insert({name:"Hollywood (Mann Theatres)",slug:"hwd",	lat:"34.102298",lon:"-118.340992"})
+			
+	db.points.insert({name:"JFK (John F. Kennedy International Airport)",slug:"jfk",lat:"40.655839",lon:"-73.807594"})
+	db.points.insert({name:"Manhattan (Grand Central Terminal)",slug:"gct",lat:"40.752466",lon:"-73.976886"})
+	db.points.insert({name:"Upper Manhattan (American Academy of Arts and Letters)",slug:"aaal",lat:"40.833721",lon:"-73.947461"})
+	db.points.insert({name:"Brooklyn (Barclays Center)",slug:"brky",lat:"40.682907",lon:"-73.975255"})
+			
+	db.points.insert({name:"SFO (San Francisco International Airport)",slug:"sfo",lat:"37.625732",lon:"-122.377807"})
+	db.points.insert({name:"Golden Gate Park (California Academy of Sciences)",slug:"acs",lat:"37.770094",lon:"-122.466031"})
+	db.points.insert({name:"Downtown SF (Powell & Market)",slug:"pwll",lat:"37.785114",lon:"-122.406677"})
+	db.points.insert({name:"Fishermans Warf (Pier 39)",slug:"warf",lat:"37.808119",lon:"-122.40911"})
 }
 
 function callUberAPI(url,start,end){
@@ -170,7 +172,6 @@ app.listen(port, function(req,res) {
   // 	points.push(res);
   // 	console.log(points);
   // })
-
+  seedDB();
   launchSchedule()
-
 });
