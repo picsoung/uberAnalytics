9ac2cd5c682b43f5c4e05d6e33b43444becb88fc
Nicolas Grenié <nicolas.grenie@gmail.com>
2014-10-17 00:42:57 -0700

    post everything to keen.io

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ var client = Keen.configure({
 });
 
 var app = express();
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
 // Connect to an NeDB database
 db.points = new Datastore({ filename: 'db/points', autoload: true });
@@ -45,32 +48,43 @@ var routes =[{start:"dtla", end:"smon"},{start:"smon", end:"dtla"},
 			 {start:"gogp", end:"warf"},{start:"warf", end:"gogp"},
 			 {start:"pwll", end:"warf"},{start:"warf", end:"pwll"}];
 
+var POINTS = [
+		{name:"Downtown LA (Walt Disney Concert Hall)",slug:"dtla",lat:"34.055515",lon:"-118.250039"},
+		{name:"Santa Monica (Third Street Promenade)",slug:"smon",lat:"34.016243",lon:"-118.496159"},
+		{name:"Hollywood (Mann Theatres)",slug:"hlwd",	lat:"34.102298",lon:"-118.340992"},
+		{name:"Manhattan (Grand Central Terminal)",slug:"grct",lat:"40.752466",lon:"-73.976886"},
+		{name:"Upper Manhattan (American Academy of Arts and Letters)",slug:"upma",lat:"40.833721",lon:"-73.947461"},
+		{name:"Brooklyn (Barclays Center)",slug:"brok",lat:"40.682907",lon:"-73.975255"},
+		{name:"Golden Gate Park (California Academy of Sciences)",slug:"gogp",lat:"37.770094",lon:"-122.466031"},
+		{name:"Downtown SF (Powell & Market)",slug:"pwll",lat:"37.785114",lon:"-122.406677"},
+		{name:"Fishermans Warf (Pier 39)",slug:"warf",lat:"37.808119",lon:"-122.40911"}]
+
 app.get('/',function(req, res){
   res.render('index');  
 });
 
 //Seed database
-app.get('/points/seed',function(req,res){
-	db.points.insert([{name:"LAX (Los Angeles International Airport)",slug:"lax",lat:"33.945452",lon:"-118.399974"},
-		{name:"Downtown LA (Walt Disney Concert Hall)",slug:"dtla",lat:"34.055515",lon:"-118.250039"},
-		{name:"Santa Monica (Third Street Promenade)",slug:"smon",lat:"34.016243",lon:"-118.496159"},
-		{name:"Hollywood (Mann Theatres)",slug:"hlwd",	lat:"34.102298",lon:"-118.340992"},
-		{name:"JFK (John F. Kennedy International Airport)",slug:"jfk",lat:"40.655839",lon:"-73.807594"},
-		{name:"Manhattan (Grand Central Terminal)",slug:"grct",lat:"40.752466",lon:"-73.976886"},
-		{name:"Upper Manhattan (American Academy of Arts and Letters)",slug:"upma",lat:"40.833721",lon:"-73.947461"},
-		{name:"Brooklyn (Barclays Center)",slug:"brok",lat:"40.682907",lon:"-73.975255"},
-		{name:"SFO (San Francisco International Airport)",slug:"sfo",lat:"37.625732",lon:"-122.377807"},
-		{name:"Golden Gate Park (California Academy of Sciences)",slug:"gogp",lat:"37.770094",lon:"-122.466031"},
-		{name:"Downtown SF (Powell & Market)",slug:"pwll",lat:"37.785114",lon:"-122.406677"},
-		{name:"Fishermans Warf (Pier 39)",slug:"warf",lat:"37.808119",lon:"-122.40911"}],function(err){
-			if(err)
-			  res.json("Errror on seeding",err);
-			else
-			  res.json("Database seeded");
+// app.get('/points/seed',function(req,res){
+// 	db.points.insert([{name:"LAX (Los Angeles International Airport)",slug:"lax",lat:"33.945452",lon:"-118.399974"},
+// 		{name:"Downtown LA (Walt Disney Concert Hall)",slug:"dtla",lat:"34.055515",lon:"-118.250039"},
+// 		{name:"Santa Monica (Third Street Promenade)",slug:"smon",lat:"34.016243",lon:"-118.496159"},
+// 		{name:"Hollywood (Mann Theatres)",slug:"hlwd",	lat:"34.102298",lon:"-118.340992"},
+// 		{name:"JFK (John F. Kennedy International Airport)",slug:"jfk",lat:"40.655839",lon:"-73.807594"},
+// 		{name:"Manhattan (Grand Central Terminal)",slug:"grct",lat:"40.752466",lon:"-73.976886"},
+// 		{name:"Upper Manhattan (American Academy of Arts and Letters)",slug:"upma",lat:"40.833721",lon:"-73.947461"},
+// 		{name:"Brooklyn (Barclays Center)",slug:"brok",lat:"40.682907",lon:"-73.975255"},
+// 		{name:"SFO (San Francisco International Airport)",slug:"sfo",lat:"37.625732",lon:"-122.377807"},
+// 		{name:"Golden Gate Park (California Academy of Sciences)",slug:"gogp",lat:"37.770094",lon:"-122.466031"},
+// 		{name:"Downtown SF (Powell & Market)",slug:"pwll",lat:"37.785114",lon:"-122.406677"},
+// 		{name:"Fishermans Warf (Pier 39)",slug:"warf",lat:"37.808119",lon:"-122.40911"}],function(err){
+// 			if(err)
+// 			  res.json("Errror on seeding",err);
+// 			else
+// 			  res.json("Database seeded");
 
-			res.end();
-		})
-});
+// 			res.end();
+// 		})
+// });
 
 app.get('/points/delete',function(req,res){
 	db.points.remove({}, { multi: true },function(err,doc){
@@ -98,9 +112,13 @@ app.get('/launch',function(req,res){
 });
 
 app.get('/point/:slug',function(req,res){
-	db.points.find({slug:req.params.slug},function(err,doc){
-		res.json(doc);
-	})
+	// db.points.find({slug:req.params.slug},function(err,doc){
+	// 	res.json(doc);
+	// })
+
+	var p = _.findWhere(POINTS,{slug:req.params.slug})
+	console.log(p)
+	res.json(p);
 });
 
 app.get('/prices/delete',function(req,res){
@@ -132,16 +150,21 @@ function getDataFromUber(){
 		url += "?"
 		async.series({
 		    startPoint: function(callback){
-		        db.points.findOne({ slug: item.start },function(err,doc){
-		        	console.log("startPOint",item.start,doc);
-					callback(null,doc);
-				});
+		    	var doc = _.findWhere(POINTS,{slug:item.start})
+		    	console.log("startPoint",doc,item.start)
+		    	callback(null,doc);
+		  //       db.points.findOne({ slug: item.start },function(err,doc){
+		  //       	console.log("startPOint",item.start,doc);
+				// 	callback(null,doc);
+				// });
 		    },
 		    endPoint: function(callback){
-		        db.points.findOne({ slug: item.end },function(err,doc){
-		        	console.log("endPOint",item.end,doc);
-					callback(null,doc);
-				});
+		    	var doc = _.findWhere(POINTS,{slug:item.end})
+		    	callback(null,doc);
+		  //       db.points.findOne({ slug: item.end },function(err,doc){
+		  //       	console.log("endPOint",item.end,doc);
+				// 	callback(null,doc);
+				// });
 		    }
 		},
 		function(err, results) {
@@ -152,7 +175,7 @@ function getDataFromUber(){
 			  	url += "&start_longitude="+results.startPoint.lon
 			  	url += "&end_latitude="
 			  	url += results.startPoint.lat;
-			  	url += "&end_longitude="+results.startPoint.lon
+			  	url += "&end_longitude="+results.endPoint.lon
 			  	url += "&server_token="+config.uber.server_token
 			  	callUberAPI(url,item.start,item.end);
 			  	console.log(url);
@@ -197,25 +220,31 @@ function callUberAPI(url,start,end){
 	    db.prices.insert({start:start,end:end,date:moment.utc().format(),prices:prices})
 	    
 	    _.each(prices,function(item){
+	    	// console.log(item)
 	    	var ev = {
 	    		start:start,
 	    		end:end,
 	    		date:moment.utc().format(),
 	    		localized_display_name:item.localized_display_name,
 	    		display_name:item.display_name,
-	    		high_estimate: item.high_estimate,
-                low_estimate: item.low_estimate,
+	    		high_estimate: parseInt(item.high_estimate),
+                low_estimate: parseInt(item.low_estimate),
 	            surge_multiplier: item.surge_multiplier,
-	            estimate: item.estimate,
-	            currency_code: item.currency_code
+	            estimate: parseInt(item.estimate.replace("$",'')),
+	            currency_code: item.currency_code,
+	            duration: item.duration,
+			    distance: item.distance
 	        }
-	        client.addEvent("pricesCollection", ev, function(err, res) {
-			    if (err) {
-			        console.log("Oh no, an error!");
-			    } else {
-			        console.log("Hooray, it worked!");
-			    }
-			});
+	        if(item.display_name != "uberTAXI"){
+		        // console.log(ev);
+			    client.addEvent("newPricesCollection", ev, function(err, res) {
+				    if (err) {
+				        console.log("Oh no, an error!",err);
+				    } else {
+				        console.log("Hooray, it worked!");
+				    }
+				});
+	        }
 	    })
 	    
 	  }
@@ -249,4 +278,12 @@ app.listen(port, function(req,res) {
   // 	console.log(points);
   // })
   // launchSchedule()
+
+  var rule = new schedule.RecurrenceRule();
+	rule.minute = new schedule.Range(0, 60, 10);
+
+	var j = schedule.scheduleJob(rule, function () {
+	  // Do something
+	  getDataFromUber();
+	});
 });
